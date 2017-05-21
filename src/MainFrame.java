0c1dd0ad3d265d77b868ec900a359c4bd9754ba2
zqhkey <zqhkey@gmail.com>
2017-05-21 16:39:00 +0800

    初始化

---
+++ src/MainFrame.java
@@ -0,0 +1,270 @@
+/**
+ * Created by zqh on 2017/5/21.
+ */
+
+import gnu.io.SerialPort;
+import gnu.io.SerialPortEvent;
+import gnu.io.SerialPortEventListener;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+
+/**
+ * 主界面
+ *
+ * @author yangle
+ */
+public class MainFrame extends JFrame {
+
+    /**
+     * 程序界面宽度
+     */
+    public static final int WIDTH = 500;
+
+    /**
+     * 程序界面高度
+     */
+    public static final int HEIGHT = 360;
+
+    private JTextArea dataView = new JTextArea();
+    private JScrollPane scrollDataView = new JScrollPane(dataView);
+
+    // 串口设置面板
+    private JPanel serialPortPanel = new JPanel();
+    private JLabel serialPortLabel = new JLabel("串口");
+    private JLabel baudrateLabel = new JLabel("波特率");
+    private JComboBox commChoice = new JComboBox();
+    private JComboBox baudrateChoice = new JComboBox();
+
+    // 操作面板
+    private JPanel operatePanel = new JPanel();
+    private JTextField dataInput = new JTextField();
+    private JButton serialPortOperate = new JButton("打开串口");
+    private JButton sendData = new JButton("发送数据");
+
+    private List<String> commList = null;
+    private SerialPort serialport;
+
+    public MainFrame() {
+        initView();
+        initComponents();
+        actionListener();
+        initData();
+    }
+
+    private void initView() {
+        // 关闭程序
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        // 禁止窗口最大化
+        setResizable(false);
+
+        // 设置程序窗口居中显示
+        Point p = GraphicsEnvironment.getLocalGraphicsEnvironment()
+                .getCenterPoint();
+        setBounds(p.x - WIDTH / 2, p.y - HEIGHT / 2, WIDTH, HEIGHT);
+        this.setLayout(null);
+
+        setTitle("串口通讯");
+    }
+
+    private void initComponents() {
+        // 数据显示
+        dataView.setFocusable(false);
+        scrollDataView.setBounds(10, 10, 475, 200);
+        add(scrollDataView);
+
+        // 串口设置
+        serialPortPanel.setBorder(BorderFactory.createTitledBorder("串口设置"));
+        serialPortPanel.setBounds(10, 220, 170, 100);
+        serialPortPanel.setLayout(null);
+        add(serialPortPanel);
+
+        serialPortLabel.setForeground(Color.gray);
+        serialPortLabel.setBounds(10, 25, 40, 20);
+        serialPortPanel.add(serialPortLabel);
+
+        commChoice.setFocusable(false);
+        commChoice.setBounds(60, 25, 100, 20);
+        serialPortPanel.add(commChoice);
+
+        baudrateLabel.setForeground(Color.gray);
+        baudrateLabel.setBounds(10, 60, 40, 20);
+        serialPortPanel.add(baudrateLabel);
+
+        baudrateChoice.setFocusable(false);
+        baudrateChoice.setBounds(60, 60, 100, 20);
+        serialPortPanel.add(baudrateChoice);
+
+        // 操作
+        operatePanel.setBorder(BorderFactory.createTitledBorder("操作"));
+        operatePanel.setBounds(200, 220, 285, 100);
+        operatePanel.setLayout(null);
+        add(operatePanel);
+
+        dataInput.setBounds(25, 25, 235, 20);
+        operatePanel.add(dataInput);
+
+        serialPortOperate.setFocusable(false);
+        serialPortOperate.setBounds(45, 60, 90, 20);
+        operatePanel.add(serialPortOperate);
+
+        sendData.setFocusable(false);
+        sendData.setBounds(155, 60, 90, 20);
+        operatePanel.add(sendData);
+    }
+
+    @SuppressWarnings("unchecked")
+    private void initData() {
+        commList = SerialPortManager.findPort();
+        // 检查是否有可用串口，有则加入选项中
+        if (commList == null || commList.size() < 1) {
+            ShowUtils.warningMessage("没有搜索到有效串口！");
+        } else {
+            for (String s : commList) {
+                commChoice.addItem(s);
+            }
+        }
+
+        baudrateChoice.addItem("9600");
+        baudrateChoice.addItem("19200");
+        baudrateChoice.addItem("38400");
+        baudrateChoice.addItem("57600");
+        baudrateChoice.addItem("115200");
+    }
+
+    private void actionListener() {
+        serialPortOperate.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if ("打开串口".equals(serialPortOperate.getText())
+                        && serialport == null) {
+                    openSerialPort(e);
+                } else {
+                    closeSerialPort(e);
+                }
+            }
+        });
+
+        sendData.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                sendData(e);
+            }
+        });
+    }
+
+    /**
+     * 打开串口
+     *
+     * @param evt
+     *            点击事件
+     */
+    private void openSerialPort(ActionEvent evt) {
+        // 获取串口名称
+        String commName = (String) commChoice.getSelectedItem();
+        // 获取波特率
+        int baudrate = 9600;
+        String bps = (String) baudrateChoice.getSelectedItem();
+        baudrate = Integer.parseInt(bps);
+
+        // 检查串口名称是否获取正确
+        if (commName == null || commName.equals("")) {
+            ShowUtils.warningMessage("没有搜索到有效串口！");
+        } else {
+                serialport = SerialPortManager.openPort(commName, baudrate);
+                if (serialport != null) {
+                    dataView.setText("串口已打开" + "\r\n");
+                    serialPortOperate.setText("关闭串口");
+                }
+        }
+            SerialPortManager.addListener(serialport, new SerialListener());
+    }
+
+    /**
+     * 关闭串口
+     *
+     * @param evt
+     *            点击事件
+     */
+    private void closeSerialPort(ActionEvent evt) {
+        SerialPortManager.closePort(serialport);
+        dataView.setText("串口已关闭" + "\r\n");
+        serialPortOperate.setText("打开串口");
+    }
+
+    /**
+     * 发送数据
+     *
+     * @param evt
+     *            点击事件
+     */
+    private void sendData(ActionEvent evt) {
+        // 输入框直接输入十六进制字符，长度必须是偶数
+        String data = dataInput.getText().toString();
+            SerialPortManager.sendToPort(serialport,
+                    ByteUtils.hexStr2Byte(data));
+    }
+
+    private class SerialListener implements SerialPortEventListener {
+        /**
+         * 处理监控到的串口事件
+         */
+        public void serialEvent(SerialPortEvent serialPortEvent) {
+
+            switch (serialPortEvent.getEventType()) {
+
+                case SerialPortEvent.BI: // 10 通讯中断
+                    ShowUtils.errorMessage("与串口设备通讯中断");
+                    break;
+
+                case SerialPortEvent.OE: // 7 溢位（溢出）错误
+
+                case SerialPortEvent.FE: // 9 帧错误
+
+                case SerialPortEvent.PE: // 8 奇偶校验错误
+
+                case SerialPortEvent.CD: // 6 载波检测
+
+                case SerialPortEvent.CTS: // 3 清除待发送数据
+
+                case SerialPortEvent.DSR: // 4 待发送数据准备好了
+
+                case SerialPortEvent.RI: // 5 振铃指示
+
+                case SerialPortEvent.OUTPUT_BUFFER_EMPTY: // 2 输出缓冲区已清空
+                    break;
+
+                case SerialPortEvent.DATA_AVAILABLE: // 1 串口存在可用数据
+                    byte[] data = null;
+                    try {
+                        if (serialport == null) {
+                            ShowUtils.errorMessage("串口对象为空！监听失败！");
+                        } else {
+                            // 读取串口数据
+                            data = SerialPortManager.readFromPort(serialport);
+                            dataView.append(ByteUtils.byteArrayToHexString(data,
+                                    true) + "\r\n");
+                        }
+                    } catch (Exception e) {
+                        ShowUtils.errorMessage(e.toString());
+                        // 发生读取错误时显示错误信息后退出系统
+                        System.exit(0);
+                    }
+                    break;
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new MainFrame().setVisible(true);
+            }
+        });
+    }
+}
---
+++ src/SerialTool.java
@@ -0,0 +1,204 @@
+import gnu.io.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.TooManyListenersException;
+
+/**
+ * Created by zqh on 2017/5/21.
+ */
+public class SerialTool {
+    private static SerialTool serialTool = null;
+
+    static {
+        //在该类被ClassLoader加载时就初始化一个SerialTool对象
+        if (serialTool == null) {
+            serialTool = new SerialTool();
+        }
+    }
+
+    //私有化SerialTool类的构造方法，不允许其他类生成SerialTool对象
+    private SerialTool() {
+    }
+
+    /**
+     * 获取提供服务的SerialTool对象
+     *
+     * @return serialTool
+     */
+    public static SerialTool getSerialTool() {
+        if (serialTool == null) {
+            serialTool = new SerialTool();
+        }
+        return serialTool;
+    }
+
+
+    /**
+     * 查找所有可用端口
+     *
+     * @return 可用端口名称列表
+     */
+    public static final ArrayList<String> findPort() {
+
+        //获得当前所有可用串口
+        Enumeration<CommPortIdentifier> portList = CommPortIdentifier.getPortIdentifiers();
+
+        ArrayList<String> portNameList = new ArrayList<>();
+
+        //将可用串口名添加到List并返回该List
+        while (portList.hasMoreElements()) {
+            String portName = portList.nextElement().getName();
+            portNameList.add(portName);
+        }
+
+        return portNameList;
+
+    }
+
+    /**
+     * 打开串口
+     *
+     * @param portName 端口名称
+     * @param baudrate 波特率
+     * @return 串口对象
+     */
+    public static final SerialPort openPort(String portName, int baudrate) {
+
+        try {
+            //通过端口名识别端口
+            CommPortIdentifier portIdentifier = CommPortIdentifier.getPortIdentifier(portName);
+
+            //打开端口，并给端口名字和一个timeout（打开操作的超时时间）
+            CommPort commPort = portIdentifier.open(portName, 2000);
+
+            //判断是不是串口
+            if (commPort instanceof SerialPort) {
+
+                SerialPort serialPort = (SerialPort) commPort;
+
+                try {
+                    //设置一下串口的波特率等参数
+                    serialPort.setSerialPortParams(baudrate, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
+                } catch (UnsupportedCommOperationException e) {
+                    e.printStackTrace();
+                }
+
+                //System.out.println("Open " + portName + " sucessfully !");
+                return serialPort;
+
+            } else {
+                System.out.print("不是串口");
+            }
+        } catch (NoSuchPortException | PortInUseException e1) {
+            e1.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 关闭串口
+     */
+    public static void closePort(SerialPort serialPort) {
+        if (serialPort != null) {
+            serialPort.close();
+            serialPort = null;
+        }
+    }
+
+    /**
+     * 往串口发送数据
+     *
+     * @param serialPort 串口对象
+     * @param order      待发送数据
+     */
+    public static void sendToPort(SerialPort serialPort, byte[] order) {
+
+        OutputStream out = null;
+
+        try {
+
+            out = serialPort.getOutputStream();
+            out.write(order);
+            out.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (out != null) {
+                try {
+                    out.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    out = null;
+                }
+            }
+        }
+
+    }
+
+    /**
+     * 从串口读取数据
+     *
+     * @param serialPort 当前已建立连接的SerialPort对象
+     * @return 读取到的数据
+     */
+    public static byte[] readFromPort(SerialPort serialPort) {
+
+        InputStream in = null;
+        byte[] bytes = null;
+
+        try {
+
+            in = serialPort.getInputStream();
+            int bufflenth = in.available();        //获取buffer里的数据长度
+
+            while (bufflenth != 0) {
+                bytes = new byte[bufflenth];    //初始化byte数组为buffer中数据的长度
+                in.read(bytes);
+                bufflenth = in.available();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (in != null) {
+                    in.close();
+                    in = null;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        return bytes;
+
+    }
+
+    /**
+     * 添加监听器
+     *
+     * @param port     串口对象
+     * @param listener 串口监听器
+     */
+    public static void addListener(SerialPort port, SerialPortEventListener listener) {
+
+        try {
+
+            //给串口添加监听器
+            port.addEventListener(listener);
+            //设置当有数据到达时唤醒监听接收线程
+            port.notifyOnDataAvailable(true);
+            //设置当通信中断时唤醒中断线程
+            port.notifyOnBreakInterrupt(true);
+
+        } catch (TooManyListenersException e) {
+            e.printStackTrace();
+        }
+    }
+}
